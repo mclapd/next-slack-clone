2652e18717b94f563b88fabc83cda9469f6a0b83
one_punch_man <ckim@banlaw.com>
2024-08-30 11:13:56 +1000

    feat: Add workspace table to Convex schema
    This commit adds a new table called "workspaces" to the Convex schema in the "convex/schema.ts" file. The "workspaces" table has columns for name, userId, and joinCode. This change allows for the storage and retrieval of workspace data in the application.

--- convex/schema.ts
+++ convex/schema.ts
@@ -4,7 +4,11 @@ import { v } from "convex/values";
 
 const schema = defineSchema({
   ...authTables,
-  // Your other tables...
+  workspaces: defineTable({
+    name: v.string(),
+    userId: v.id("users"),
+    joinCode: v.string(),
+  }),
 });
 
 export default schema;
---
+++ src/features/workspaces/components/create-workspace-modal.tsx
@@ -0,0 +1,63 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import { useCreateWorkspaceModal } from "../store/use-create-workspace-modal";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useCreateWorkspace } from "../api/use-create-workspace";
+import { useState } from "react";
+
+export const CreateWorkspaceModal = () => {
+  const [open, setOpen] = useCreateWorkspaceModal();
+  const [name, setName] = useState("");
+
+  const { mutate, isPending } = useCreateWorkspace();
+
+  const handleClose = () => {
+    setOpen(false);
+    // TODO: clear form
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    mutate(
+      { name },
+      {
+        onSuccess(data) {
+          console.log(data);
+        },
+      }
+    );
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={handleClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add a workspace</DialogTitle>
+        </DialogHeader>
+        <DialogDescription>Add workspace blah blah</DialogDescription>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <Input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            disabled={isPending}
+            required
+            autoFocus
+            minLength={3}
+            placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
+          />
+          <div className="flex justify-end">
+            <Button disabled={isPending}>Create</Button>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
