b8c156d9a4124040c3044595e61d62289cfcfc82
one_punch_man <ckim@banlaw.com>
2024-09-05 22:28:52 +1000

    feat: Add create channel mutation and modal component
    This commit adds a new mutation called "create" to the channels API in the "convex/channels.ts" file. The "create" mutation allows creating a new channel with a specified name and workspace ID. Additionally, a new modal component called "CreateChannelModal" is added to the "src/features/channels/components" directory. This modal provides a user interface for creating a new channel. These changes enhance the functionality of the application by enabling the creation of channels within workspaces.

--- convex/channels.ts
+++ convex/channels.ts
@@ -1,6 +1,37 @@
 import { v } from "convex/values";
-import { query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { getAuthUserId } from "@convex-dev/auth/server";
+
+export const create = mutation({
+  args: { name: v.string(), workspaceId: v.id("workspaces") },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const members = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.workspaceId).eq("userId", userId)
+      )
+      .unique();
+
+    if (!members || members.role !== "admin") {
+      throw new Error("Unauthorized");
+    }
+
+    const parseName = args.name.replace(/\s/g, "-").toLowerCase();
+
+    const channelId = await ctx.db.insert("channels", {
+      name: parseName,
+      workspaceId: args.workspaceId,
+    });
+
+    return channelId;
+  },
+});
 
 export const get = query({
   args: { workspaceId: v.id("workspaces") },
---
+++ src/features/channels/components/create-channel-modal.tsx
@@ -0,0 +1,61 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useCreateChannelModal } from "../store/use-create-channel-modal";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { useCreateChannel } from "../api/use-create-channel";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+
+export const CreateChannelModal = () => {
+  const workspaceId = useWorkspaceId();
+  const [open, setOpen] = useCreateChannelModal();
+  const { mutate, isPending } = useCreateChannel();
+  const [name, setName] = useState("");
+
+  const handleClose = () => {
+    setName("");
+    setOpen(false);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.replace(/\s+/g, "-").toLowerCase();
+    setName(value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    mutate({ name, workspaceId }, { onSuccess: (id) => handleClose() }); // TODO: Refetch channels
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={handleClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add a channel</DialogTitle>
+        </DialogHeader>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <Input
+            value={name}
+            disabled={isPending}
+            onChange={handleChange}
+            required
+            autoFocus
+            minLength={3}
+            maxLength={80}
+            placeholder="e.g. plan-budget"
+          />
+          <div className="flex justify-end">
+            <Button disabled={isPending}>Create channel</Button>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
