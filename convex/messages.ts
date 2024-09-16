8a47f4f3ab9c9cd7bd62a10442785b09e3579cf9
one_punch_man <ckim@banlaw.com>
2024-09-16 15:23:31 +1000

    refactor: Add scrollbar styling to messages
    - Added CSS styles for scrollbar in the messages module
    - Updated globals.css to include styles for the scrollbar

--- convex/messages.ts
+++ convex/messages.ts
@@ -100,9 +100,9 @@ export const get = query({
 
     return {
       ...results,
-      page: await Promise.all(
-        results.page
-          .map(async (message) => {
+      page: (
+        await Promise.all(
+          results.page.map(async (message) => {
             const member = await populateMember(ctx, message.memberId);
             const user = member ? await populateUser(ctx, member.userId) : null;
 
@@ -160,10 +160,9 @@ export const get = query({
               threadTimeStamp: thread.timeStamp,
             };
           })
-          .filter(
-            (message): message is NonNullable<typeof message> =>
-              message !== null
-          )
+        )
+      ).filter(
+        (message): message is NonNullable<typeof message> => message !== null
       ),
     };
   },
