0c8d6d827e0d129d4b9b4cb758dfec60efcbae47
one_punch_man <ckim@banlaw.com>
2024-09-17 14:10:00 +1000

    refactor: Add toolbar component to message component

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -4,6 +4,7 @@ import { format, isToday, isYesterday } from "date-fns";
 import { Hint } from "./hint";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Thumbnail } from "./thumbnail";
+import { Toolbar } from "./toolbar";
 
 const Renderer = dynamic(() => import("@/components/renderer"), { ssr: false });
 
@@ -106,6 +107,19 @@ export const Message = ({
             <span className="text-xs text-muted-foreground">(edited)</span>
           ) : null}
         </div>
+      </div>
+      <div>
+        {!isEditing && (
+          <Toolbar
+            isAuthor={isAuthor}
+            isPending={false}
+            handleEdit={() => setEditingId(id)}
+            handleThread={() => {}}
+            handleDelete={() => {}}
+            handleReaction={() => {}}
+            hideThreadButton={hideThreadButton}
+          />
+        )}
       </div>
     </div>
   );
