6202da8b8e0b2fdaa94cc6fb8f516568cb7d8e71
one_punch_man <ckim@banlaw.com>
2024-09-22 22:40:28 +1000

    feat: Add use-member-id hook for retrieving member ID

---
+++ src/components/conversation-hero.tsx
@@ -0,0 +1,25 @@
+import { format } from "date-fns";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+interface ConversationHeroProps {
+  name?: string;
+  image?: string;
+}
+
+export const ConversationHero = ({
+  name = "Member",
+  image,
+}: ConversationHeroProps) => {
+  const avatarFallback = name.charAt(0).toUpperCase();
+
+  return (
+    <div className="mt-[88px] mx-5 mb-4">
+      <div className="flex items-center gap-x-1 mb-2">
+        <Avatar className="size-14 mr-2">
+          <AvatarImage src={image} />
+          <AvatarFallback>{avatarFallback}</AvatarFallback>
+        </Avatar>
+      </div>
+    </div>
+  );
+};
